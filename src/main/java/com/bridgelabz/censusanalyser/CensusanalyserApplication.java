4a2c98a1cb2796bb4357e251b2ff815adbb8782b
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-25 15:04:23 +0530

    Add: welcome message for Census Analyser program.

---
+++ src/main/java/com/bridgelabz/censusanalyser/CensusanalyserApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.censusanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensusanalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensusanalyserApplication.class, args);
+	}
+
+}
